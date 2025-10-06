2bb26315fa889041f32e6ce2b58570ff5e39b108
Pere <peremarquez2000@gmail.com>
2025-10-06 09:42:40 +0200

    add bonus functions + modify makefile

---
+++ cursus/libft_general/libft/ft_lstadd_back.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                      		        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pemarque <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/19 13:22:43 by pemarque          #+#    #+#             */
+/*   Updated: 2025/09/25 13:08:23 by pemarque         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (*lst == NULL)
+		*lst = new;
+	else 
+		ft_lstlast(*lst)->next = new;
+}
+/*
+int main(void)
+{
+	char *a = "hola";
+	char *b = "adeu";
+	char *c = "bombo";
+	t_list *p1;
+	t_list *p2;
+	t_list *p3;
+	p1 = ft_lstnew(a);
+	p2 = ft_lstnew(b);
+	p3 = ft_lstnew(c);
+	ft_lstadd_front(&p2,p1);
+	ft_lstadd_back(&p2,p3);
+	
+	printf("\n%s", (char *)ft_lstlast(p1)->content);
+
+}
+	*/
--- cursus/libft_general/libft/libft.h
+++ cursus/libft_general/libft/libft.h
@@ -13,6 +13,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+typedef struct s_list
+{
+	void			*content;
+	struct s_list	*next;
+}	t_list;
+
 int		ft_atoi(const char *str);
 void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t nmemb, size_t size);
@@ -47,3 +53,13 @@ char	*ft_strtrim(char const *s1, char const *set);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 int		ft_tolower(int c);
 int		ft_toupper(int c);
+
+//BONUS
+
+t_list	*ft_lstnew(void *content);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+int		ft_lstsize(t_list *lst);
+t_list	*ft_lstlast(t_list *lst);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+
