787b4d06a8068e89672757ed84fd14b529092fcb
Pere <peremarquez2000@gmail.com>
2025-10-28 17:02:36 +0100

    fix: fix libft.h norminette

--- cursus/printf_general/ft_printf/ft_printf.h
+++ cursus/printf_general/ft_printf/ft_printf.h
@@ -1,21 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pemarque <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/19 13:22:43 by pemarque          #+#    #+#             */
+/*   Updated: 2025/08/29 10:46:57 by pemarque         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
-#define FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdarg.h>
+# include "libft/libft.h"
 
-#include <stdarg.h>
-#include "libft/libft.h"
-
-int ft_printf(char const *, ...);
+int			ft_printf(char const *string, ...);
 
 // FT_PRINT_UTILS
-int ft_cspdiux(char c, va_list args);
-int ft_digits(int number);
-int ft_digits_ui(unsigned int number);
-void ft_putnbr_ui_fd(unsigned int n, int fd);
+int			ft_cspdiux(char c, va_list args);
+int			ft_digits(int number);
+int			ft_digits_ui(unsigned int number);
+void		ft_putnbr_ui_fd(unsigned int n, int fd);
 
 // FT_HEXA
-void ft_hexa(unsigned int n, int flag);
-int ft_len_hexa(unsigned long n);
-int ft_print_from_ui_to_hexa(unsigned int n, int flag);
-int ft_hexa_adress(unsigned long n);
+void		ft_hexa(unsigned int n, int flag);
+int			ft_len_hexa(unsigned long n);
+int			ft_print_from_ui_to_hexa(unsigned int n, int flag);
+int			ft_hexa_adress(unsigned long n);
 
 #endif
