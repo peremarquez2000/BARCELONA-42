4297879cf4c9f27f8c6b93af6fc0a0257912a6b2
Pere <peremarquez2000@gmail.com>
2025-10-31 10:28:55 +0100

    fix: fixing norminette again

--- cursus/get_next_line_general/get_next_line/get_next_line.c
+++ cursus/get_next_line_general/get_next_line/get_next_line.c
@@ -16,43 +16,44 @@
 
 char	*get_next_line(int fd)
 {
-	char		*nl = NULL;
+	char		*nl;
 	int			nl_size;
 	static char	*st_buffer = NULL;
-	static int	st_bytesRead = 0;
+	static int	st_bytesread = 0;
 	char		*buffer;
-	int			bytesRead;
+	int			bytesread;
 	int			buffer_size;
 	int			posicion_barra_n;
 	int			flag;
 
+	nl = NULL;
 	nl_size = 0;
 	flag = 1;
 	buffer_size = BUFFER_SIZE;
-	bytesRead = 0;
+	bytesread = 0;
 	buffer = (char *)malloc(buffer_size * sizeof(char));
 	if (!buffer)
 		return (NULL);
 	while (flag)
 	{
-		bytesRead = read(fd, buffer, buffer_size);
-		st_buffer = ft_new_buff(st_buffer, st_bytesRead, buffer, bytesRead);
-		st_bytesRead += bytesRead;
-		if (bytesRead == 0 && st_bytesRead == 0)
+		bytesread = read(fd, buffer, buffer_size);
+		st_buffer = ft_new_buff(st_buffer, st_bytesread, buffer, bytesread);
+		st_bytesread += bytesread;
+		if (bytesread == 0 && st_bytesread == 0)
 			return (NULL);
-		posicion_barra_n = ft_bar_n_position(st_buffer, st_bytesRead);
+		posicion_barra_n = ft_bar_n_position(st_buffer, st_bytesread);
 		if (posicion_barra_n != -1)
 		{
 			nl = ft_new_nl(nl, nl_size, st_buffer, posicion_barra_n);
-			st_buffer = ft_tail(st_buffer, st_bytesRead, posicion_barra_n + 1);
-			st_bytesRead = st_bytesRead - posicion_barra_n - 1;
+			st_buffer = ft_tail(st_buffer, st_bytesread, posicion_barra_n + 1);
+			st_bytesread = st_bytesread - posicion_barra_n - 1;
 			flag = 0;
 		}
-		else if (st_bytesRead != buffer_size && bytesRead != buffer_size)
+		else if (st_bytesread != buffer_size && bytesread != buffer_size)
 		{
 			nl = st_buffer;
 			st_buffer = NULL;
-			st_bytesRead = 0;
+			st_bytesread = 0;
 			flag = 0;
 		}
 	}
@@ -61,50 +62,50 @@ char	*get_next_line(int fd)
 /* 
 int main()
 {
-    char *fileName = "test.txt";
-    // char* nl;
-    int fd = open(fileName, O_RDONLY);
-    if (fd == -1)
-    {
-        printf("\nError Opening File!!\n");
-        return (1);
-    }
-    else
-        printf("\nFile \"%s\" opened sucessfully!\n", fileName);
+	char *fileName = "test.txt";
+	// char* nl;
+	int fd = open(fileName, O_RDONLY);
+	if (fd == -1)
+	{
+		printf("\nError Opening File!!\n");
+		return (1);
+	}
+	else
+		printf("\nFile \"%s\" opened sucessfully!\n", fileName);
 
-    for (int i = 0; i < 4; i++)
-    {
-        printf("\ni = %d\n", i);
-        printf("%s", get_next_line(fd));
-    }
-    return (0);
+	for (int i = 0; i < 4; i++)
+	{
+		printf("\ni = %d\n", i);
+		printf("%s", get_next_line(fd));
+	}
+	return (0);
 } */
 /* int main()
 {
-    char* fileName = "test.txt";
-    // char* nl;
-    int fd = open(fileName, O_RDONLY);
-    if(fd == -1)
-    {
-        printf("\nError Opening File!!\n");
-        return(1);
-    }
-    else
-        printf("\nFile \"%s\" opened sucessfully!\n", fileName);
+	char* fileName = "test.txt";
+	// char* nl;
+	int fd = open(fileName, O_RDONLY);
+	if(fd == -1)
+	{
+		printf("\nError Opening File!!\n");
+		return(1);
+	}
+	else
+		printf("\nFile \"%s\" opened sucessfully!\n", fileName);
 
-    printf("%s",get_next_line(fd));
-    return(0);
+	printf("%s",get_next_line(fd));
+	return(0);
 } */
 /*
 int main(void)
 {
-    char *line;
+	char *line;
 
-    while ((line = get_next_line(0)) != NULL)
-    {
-        printf("%s", line);
-        free(line);
-    }
-    return 0;
+	while ((line = get_next_line(0)) != NULL)
+	{
+		printf("%s", line);
+		free(line);
+	}
+	return 0;
 }
 */
